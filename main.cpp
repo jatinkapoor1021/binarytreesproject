45c018bfcf7dda1e07021d19a70636c1d4ac7201
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2024-04-05 15:55:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "myVector.hpp"
+#include "myList.hpp"
+#include "myHeap.hpp"
+#include "myAVLtree.hpp"
+
+using namespace std;
+
+int main() {
+    ifstream inputFile("input.txt");
+    vector<string> instructions;
+    string instruction;
+    
+    // Read instructions from file
+    while (getline(inputFile, instruction)) {
+        instructions.push_back(instruction);
+    }
+
+    // Using vector method
+    vectorMedian(&instructions);
+
+    // Using list method
+    listMedian(&instructions);
+
+    // Using heap method
+    heapMedian(&instructions);
+
+    // Using AVL tree method
+    treeMedian(&instructions);
+
+    return 0;
+}
