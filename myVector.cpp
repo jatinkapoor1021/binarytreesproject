0d61a46589284db4c6cc7e090b19213c15f5245d
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2024-04-05 16:05:32 -0400

    Update myVector.cpp

--- myVector.cpp
+++ myVector.cpp
@@ -1,1 +1,17 @@
+#include "myVector.hpp"
+#include <algorithm>
 
+void vectorMedian(const std::vector<std::string>* instructions) {
+    std::vector<int> vec;
+    for (const std::string& instruction : *instructions) {
+        if (instruction == "pop median") {
+            std::sort(vec.begin(), vec.end());
+            int median = vec[vec.size() / 2];
+            std::cout << median << " ";
+            vec.erase(std::remove(vec.begin(), vec.end(), median), vec.end());
+        } else {
+            int value = std::stoi(instruction.substr(7)); // Extract value from instruction
+            vec.push_back(value);
+        }
+    }
+}
