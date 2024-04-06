7be0891e8f7e6c6dbe2edd3be9eca6712510c62b
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2024-04-06 15:53:20 -0400

    Update myHeap.cpp

--- myHeap.cpp
+++ myHeap.cpp
@@ -1,34 +1,37 @@
-#include "myHeap.hpp"
 #include <queue>
+#include "myHeap.hpp"
 
-void heapMedian(const std::vector<std::string>* instructions) {
-    std::priority_queue<int> small; // Max heap
-    std::priority_queue<int, std::vector<int>, std::greater<int>> large; // Min heap
-    for (const std::string& instruction : *instructions) {
-        if (instruction == "pop median") {
-            int median = small.top();
-            std::cout << median << " ";
-            small.pop();
-            // Rebalance if necessary
-            if (small.size() < large.size()) {
-                small.push(large.top());
-                large.pop();
-            }
-        } else {
-            int value = std::stoi(instruction.substr(7)); // Extract value from instruction
-            if (small.empty() || value <= small.top()) {
-                small.push(value);
+void heapMedian(const std::vector<int>* instructions) {
+    std::priority_queue<int> small;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> large;
+    std::vector<int> medians;
+    for (int instruction : *instructions) {
+        if (instruction > 0) { // Insert
+            if (small.empty() || instruction <= small.top()) {
+                small.push(instruction);
             } else {
-                large.push(value);
+                large.push(instruction);
             }
-            // Rebalance if necessary
-            if (small.size() > large.size() + 1) {
-                large.push(small.top());
-                small.pop();
-            } else if (small.size() < large.size()) {
-                small.push(large.top());
-                large.pop();
-            }
+            rebalance(small, large);
+        } else { // Pop median
+            medians.push_back(small.top());
+            small.pop();
+            rebalance(small, large);
         }
+    }
+    // Print medians
+    for (int median : medians) {
+        std::cout << median << " ";
+    }
+    std::cout << std::endl;
+}
+
+void rebalance(std::priority_queue<int>& small, std::priority_queue<int, std::vector<int>, std::greater<int>>& large) {
+    if (small.size() > large.size() + 1) {
+        large.push(small.top());
+        small.pop();
+    } else if (small.size() < large.size()) {
+        small.push(large.top());
+        large.pop();
     }
 }
