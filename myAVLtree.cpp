816d545247982cd28e67205f75dab96810cd52d9
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2024-04-06 15:53:34 -0400

    Update myAVLtree.cpp

--- myAVLtree.cpp
+++ myAVLtree.cpp
@@ -1,44 +1,26 @@
 #include "myAVLtree.hpp"
-#include <map>
 
-void treeMedian(const std::vector<std::string>* instructions) {
-    std::map<int, int> smallTree; // AVL tree for elements less than or equal to the median
-    std::map<int, int> largeTree; // AVL tree for elements greater than the median
-    for (const std::string& instruction : *instructions) {
-        if (instruction == "pop median") {
-            auto it = smallTree.end();
-            --it;
-            int median = it->first;
-            std::cout << median << " ";
-            if (it->second == 1) {
-                smallTree.erase(it);
+void treeMedian(const std::vector<int>* instructions) {
+    AVLTree small;
+    AVLTree large;
+    std::vector<int> medians;
+    for (int instruction : *instructions) {
+        if (instruction > 0) { // Insert
+            if (small.isEmpty() || instruction <= small.getMedian()) {
+                small.insert(instruction);
             } else {
-                it->second--;
+                large.insert(instruction);
             }
-            // Rebalance if necessary
-            if (smallTree.size() < largeTree.size()) {
-                auto it2 = largeTree.begin();
-                smallTree.insert(*it2);
-                largeTree.erase(it2);
-            }
-        } else {
-            int value = std::stoi(instruction.substr(7)); // Extract value from instruction
-            if (smallTree.empty() || value <= smallTree.rbegin()->first) {
-                smallTree[value]++;
-            } else {
-                largeTree[value]++;
-            }
-            // Rebalance if necessary
-            if (smallTree.size() > largeTree.size() + 1) {
-                auto it = smallTree.end();
-                --it;
-                largeTree.insert(*it);
-                smallTree.erase(it);
-            } else if (smallTree.size() < largeTree.size()) {
-                auto it = largeTree.begin();
-                smallTree.insert(*it);
-                largeTree.erase(it);
-            }
+            rebalance(small, large);
+        } else { // Pop median
+            medians.push_back(small.getMedian());
+            small.removeMedian();
+            rebalance(small, large);
         }
     }
+    // Print medians
+    for (int median : medians) {
+        std::cout << median << " ";
+    }
+    std::cout << std::endl;
 }
