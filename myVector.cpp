ae6d3b22516d561ac026c6dbd5e74fd7d7a08997
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2024-04-06 15:52:42 -0400

    Update myVector.cpp

--- myVector.cpp
+++ myVector.cpp
@@ -1,17 +1,23 @@
-#include "myVector.hpp"
+#include <vector>
 #include <algorithm>
+#include "myVector.hpp"
 
-void vectorMedian(const std::vector<std::string>* instructions) {
-    std::vector<int> vec;
-    for (const std::string& instruction : *instructions) {
-        if (instruction == "pop median") {
-            std::sort(vec.begin(), vec.end());
-            int median = vec[vec.size() / 2];
-            std::cout << median << " ";
-            vec.erase(std::remove(vec.begin(), vec.end(), median), vec.end());
-        } else {
-            int value = std::stoi(instruction.substr(7)); // Extract value from instruction
-            vec.push_back(value);
+void vectorMedian(const std::vector<int>* instructions) {
+    std::vector<int> data;
+    std::vector<int> medians;
+    for (int instruction : *instructions) {
+        if (instruction > 0) { // Insert
+            auto it = std::lower_bound(data.begin(), data.end(), instruction);
+            data.insert(it, instruction);
+        } else { // Pop median
+            int medianIndex = data.size() / 2;
+            medians.push_back(data[medianIndex]);
+            data.erase(data.begin() + medianIndex);
         }
     }
+    // Print medians
+    for (int median : medians) {
+        std::cout << median << " ";
+    }
+    std::cout << std::endl;
 }
